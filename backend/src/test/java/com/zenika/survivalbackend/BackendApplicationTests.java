3f987b2ec4cb928c6ebff5935d80689713a6f1ba
Alberto Moreno <alberto.moreno.ep@whoz.com>
2022-06-03 10:16:15 +0200

    Initial empty backend created

---
+++ backend/src/test/java/com/zenika/survivalbackend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenika.survivalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
