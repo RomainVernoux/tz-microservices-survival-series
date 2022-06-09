28e0d044d8881b3dd1ff9686b7c01a7a81d39e0a
Alberto Moreno <alberto.moreno.ep@whoz.com>
2022-06-09 16:58:48 +0200

    Copying backend to backend-ddd

---
+++ backend-ddd/src/main/java/com/zenika/survivalbackend/repository/UserStoryRepository.java
@@ -0,0 +1,14 @@
+package com.zenika.survivalbackend.repository;
+
+import com.zenika.survivalbackend.model.UserStory;
+import com.zenika.survivalbackend.model.UserStoryStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserStoryRepository extends JpaRepository<UserStory, UUID> {
+
+    long countByProjectIdAndUserStoryStatusAndIdNot(UUID projectId, UserStoryStatus status, UUID userStoryId);
+}
