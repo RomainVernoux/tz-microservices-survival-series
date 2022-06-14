3dc2ffd71121415075bde3a03fa6e8e7482d8fff
Alberto Moreno <alberto.moreno.ep@whoz.com>
2022-06-14 20:13:08 +0200

    Correcting errors

--- backend-saga/user-story-service-saga/src/main/java/com/zenika/survivalbackend/exposition/UserStoryController.java
+++ backend-saga/user-story-service-saga/src/main/java/com/zenika/survivalbackend/exposition/UserStoryController.java
@@ -23,7 +23,7 @@ public class UserStoryController {
         return userStoryService.getAllUserStories();
     }
 
-    @PutMapping("/{id}/change-status")
+    @PostMapping("/{id}/change-status")
     public void changeUserStoryStatus(@PathVariable UUID id, @RequestBody ChangeUserStoryStatusDto dto) {
         userStoryService.changeUserStoryStatus(id, UserStoryStatus.valueOf(dto.newStatus()));
     }
